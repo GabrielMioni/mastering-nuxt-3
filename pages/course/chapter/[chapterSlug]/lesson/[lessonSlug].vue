8acec1dd353bb2d17e94be1718fcec33111cae63
Gabriel Mioni <gabriel.mioni@gmail.com>
2024-09-25 16:09:22 -0700

    Lesson 1-12

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -1,6 +1,50 @@
 <template>
-  <h2>Lesson</h2>
-  <p>This is a lesson</p>
-  <p>Chapter slug: {{ $route.params.chapterSlug }}</p>
-  <p>Lesson slug: {{ $route.params.lessonSlug }}</p>
+  <div>
+    <p class="mt-0 uppercase font-bold text-slate-400 mb-1">
+      Lesson {{ chapter.number }} - {{ lesson.number }}
+    </p>
+    <h2 class="my-0">{{ lesson.title }}</h2>
+    <div class="flex space-x-4 mt-2 mb-8">
+      <a
+        v-if="lesson.sourceUrl"
+        class="font-normal text-md text-gray-500"
+        :href="lesson.sourceUrl"
+      >
+        Download Source Code
+      </a>
+      <video-player :video-id="lesson.videoId"/>
+    </div>
+    <p>{{ lesson.text }}</p>
+  </div>
 </template>
+
+<script setup>
+const course = useCourse();
+const route = useRoute();
+
+const chapter = computed(() => {
+  return course.chapters.find(
+    (chapter) => chapter.slug === route.params.chapterSlug
+  );
+});
+
+const lesson = computed(() => {
+  return chapter.value.lessons.find(
+    (lesson) => lesson.slug === route.params.lessonSlug
+  );
+});
+
+const title = computed(() => `${lesson.value.title} - ${course.title}`);
+
+useHead({
+  // title: `${lesson.value.title} - Mastering Nuxt 3`,
+  title: title.value,
+  meta: [
+    {
+      name: 'description',
+      content: lesson.value.text,
+    },
+  ],
+});
+
+</script>
