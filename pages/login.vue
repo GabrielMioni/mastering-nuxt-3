b8d1844407a108fe0ab30adbc73c734684938be5
Gabriel Mioni <gabriel.mioni@gmail.com>
2024-10-01 11:11:17 -0700

    Trouble integrating supabase

---
+++ pages/login.vue
@@ -0,0 +1,35 @@
+<script setup>
+// const { title } = useCourse()
+
+const supabase = useSupabaseClient()
+
+const doThing = () => {
+  console.log('thing')
+}
+
+const login = async () => {
+  const { error } = await supabase.auth.signInWithOAuth({
+    provider: 'github',
+  })
+
+  if (error) {
+    console.error('Error logging in:', error.message)
+  }
+}
+</script>
+
+
+<template>
+  <div class="prose w-full max-w-2xl h-9">
+<!--    <h1>Log in to {{ title }}</h1>-->
+    <button @click="doThing">
+      Bringo
+    </button>
+    <button
+      class="bg-blue-500 text-white font-bold py-2 px-4 rounded"
+      @click="login"
+    >
+      Log in with Github
+    </button>
+  </div>
+</template>
